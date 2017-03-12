ec010cf39d9872be640da2e99aa9ea088743c871
nannan <nannan@163.com>
2017-03-12 22:52:00 +0800

    添加user管理dao

---
+++ vending/src/main/java/com/vending/platform/dao/sqlprovider/UserManagerSqlProvider.java
@@ -0,0 +1,319 @@
+package com.vending.platform.dao.sqlprovider;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.jdbc.SQL;
+
+import com.vending.platform.domain.AuthorityInfo;
+import com.vending.platform.domain.RoleInfo;
+import com.vending.platform.domain.UserInfo;
+
+/** @author Miley_Ren */
+public class UserManagerSqlProvider {
+
+	public String insertAuthorityInfo(AuthorityInfo authorityInfo) {
+		return new SQL() {
+			{
+				INSERT_INTO("authorityinfo");
+				if (StringUtils.isNotBlank(authorityInfo.getAuthName())) {
+					VALUES("authName", "#{authName}");
+				}
+				if (StringUtils.isNotBlank(authorityInfo.getAuthCode())) {
+					VALUES("authCode", "#{authCode}");
+				}
+				if (authorityInfo.getFirmId() != null) {
+					VALUES("firmId", "#{firmId}");
+				}
+				if (authorityInfo.getOperateId() != null) {
+					VALUES("operateId", "#{operateId}");
+				}
+				VALUES("operateDate", "(select now())");
+			}
+		}.toString();
+	}
+
+	public String updateAutorityInfo(AuthorityInfo authorityInfo) {
+		return new SQL() {
+			{
+				if (authorityInfo.getAuthId() != null) {
+					UPDATE("authorityinfo");
+					if (StringUtils.isNotBlank(authorityInfo.getAuthName())) {
+						SET("authName=#{authName}");
+					}
+					if (StringUtils.isNotBlank(authorityInfo.getAuthCode())) {
+						SET("authCode=#{authCode}");
+					}
+					if (authorityInfo.getFirmId() != null) {
+						SET("firmId=#{firmId}");
+					}
+					if (authorityInfo.getOperateId() != null) {
+						SET("operateId=#{operateId}");
+					}
+					SET("operateDate=(select now())");
+					WHERE("authId=#{authId}");
+				}
+			}
+		}.toString();
+	}
+
+	public String getAllAuthorityInfos(AuthorityInfo authorityInfo) {
+		return new SQL() {
+			{
+				if (authorityInfo != null) {
+					SELECT("*").FROM("authorityinfo");
+					if (StringUtils.isNotBlank(authorityInfo.getAuthName())) {
+						WHERE("authName=#{authName}");
+					}
+					if (StringUtils.isNotBlank(authorityInfo.getAuthCode())) {
+						WHERE("authCode=#{authCode}");
+					}
+					if (authorityInfo.getFirmId() != null) {
+						WHERE("firmId=#{firmId}");
+					}
+				}
+			}
+		}.toString();
+	}
+
+	public String getAuthorityInfoById() {
+		return "SELECT * FROM authorityinfo WHERE authId=#{authId}";
+	}
+
+	public String deleteAuthorityInfo() {
+		return "DELETE FROM authorityinfo WHERE authId=#{authId}";
+	}
+
+	public String insertRoleInfo(RoleInfo roleInfo) {
+		return new SQL() {
+			{
+				INSERT_INTO("roleinfo");
+				if (StringUtils.isNotBlank(roleInfo.getRoleName())) {
+					VALUES("roleName", "#{roleName}");
+				}
+				if (StringUtils.isNotBlank(roleInfo.getAuthorityCode())) {
+					VALUES("authorityCode", "#{authorityCode}");
+				}
+				if (StringUtils.isNotBlank(roleInfo.getAuthorityName())) {
+					VALUES("authorityName", "#{authorityName}");
+				}
+				if (roleInfo.getStatus() != null) {
+					VALUES("status", "#{status}");
+				}
+				if (roleInfo.getFirmId() != null) {
+					VALUES("firmId", "#{firmId}");
+				}
+				if (roleInfo.getOperateId() != null) {
+					VALUES("operateId", "#{operateId}");
+				}
+				VALUES("operateDate", "(SELECT NOW())");
+			}
+		}.toString();
+	}
+
+	public String updateRoleInfo(RoleInfo roleInfo) {
+		return new SQL() {
+			{
+				if (roleInfo.getRoleId() != null) {
+					UPDATE("roleinfo");
+					if (StringUtils.isNotBlank(roleInfo.getRoleName())) {
+						SET("roleName=#{roleName}");
+					}
+					if (StringUtils.isNotBlank(roleInfo.getAuthorityCode())) {
+						SET("authorityCode=#{authorityCode}");
+					}
+					if (StringUtils.isNotBlank(roleInfo.getAuthorityName())) {
+						SET("authorityName=#{authorityName}");
+					}
+					if (roleInfo.getStatus() != null) {
+						SET("status=#{status}");
+					}
+					if (roleInfo.getFirmId() != null) {
+						SET("firmId =#{firmId}");
+					}
+					if (roleInfo.getOperateId() != null) {
+						SET("operateId=#{operateId}");
+					}
+					SET("operateDate=(SELECT NOW())");
+					WHERE("roleId=#{roleId}");
+				}
+			}
+		}.toString();
+	}
+
+	public String getAllRoles(RoleInfo roleInfo) {
+		return new SQL() {
+			{
+				SELECT("*").FROM("roleinfo");
+				if (StringUtils.isNotBlank(roleInfo.getRoleName())) {
+					WHERE("roleName=#{roleName}");
+				}
+				if (StringUtils.isNotBlank(roleInfo.getAuthorityCode())) {
+					WHERE("authorityCode=#{authorityCode}");
+				}
+				if (StringUtils.isNotBlank(roleInfo.getAuthorityName())) {
+					WHERE("authorityName=#{authorityName}");
+				}
+				if (roleInfo.getStatus() != null) {
+					WHERE("status=#{status}");
+				}
+				if (roleInfo.getFirmId() != null) {
+					WHERE("firmId =#{firmId}");
+				}
+				if (roleInfo.getOperateId() != null) {
+					WHERE("operateId=#{operateId}");
+				}
+			}
+		}.toString();
+	}
+
+	public String getRoleById(Integer roleId) {
+		return "select * from roleinfo where roleId=#{roleinfo}";
+	}
+
+	public String deleteRole(Integer roleId) {
+		return "delete from roleinfo where roleId=#{roleId}";
+	}
+
+	public String insertUser(UserInfo userInfo) {
+		return new SQL() {
+			{
+				INSERT_INTO("userinfo");
+				if (userInfo.getUserNo() != null) {
+					VALUES("userNo", "#{userNo}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getUserName())) {
+					VALUES("userName", "#{userName}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getPassword())) {
+					VALUES("password", "#{password}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getMobilePhone())) {
+					VALUES("mobilePhone", "#{mobilePhone}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getEmail())) {
+					VALUES("email", "#{email}");
+				}
+				if (userInfo.getRoleId() != null) {
+					VALUES("roleId", "#{roleId}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getRoleName())) {
+					VALUES("roleName", "#{roleName}");
+				}
+				if (userInfo.getGroupId() != null) {
+					VALUES("groupId", "#{groupId}");
+				}
+				if (userInfo.getStatus() != null) {
+					VALUES("status", "#{status}");
+				}
+				if (userInfo.getFirmId() != null) {
+					VALUES("firmId", "#{firmId}");
+				}
+				if (userInfo.getParentUserId() != null) {
+					VALUES("parentUserId", "#{parentUserId}");
+				}
+				if (userInfo.getOperateId() != null) {
+					VALUES("operateId", "#{operateId}");
+				}
+				VALUES("operateDate", "(SELECT NOW())");
+			}
+		}.toString();
+	}
+
+	public String updateUser(UserInfo userInfo) {
+		return new SQL() {
+			{
+				if (userInfo.getUserId() != null) {
+					UPDATE("userinfo");
+					if (userInfo.getUserNo() != null) {
+						SET("userNo=#{userNo}");
+					}
+					if (StringUtils.isNotBlank(userInfo.getUserName())) {
+						SET("userName=#{userName}");
+					}
+					if (StringUtils.isNotBlank(userInfo.getPassword())) {
+						SET("password=#{password}");
+					}
+					if (StringUtils.isNotBlank(userInfo.getMobilePhone())) {
+						SET("mobilePhone=#{mobilePhone}");
+					}
+					if (StringUtils.isNotBlank(userInfo.getEmail())) {
+						SET("email=#{email}");
+					}
+					if (userInfo.getRoleId() != null) {
+						SET("roleId=#{roleId}");
+					}
+					if (StringUtils.isNotBlank(userInfo.getRoleName())) {
+						SET("roleName=#{roleName}");
+					}
+					if (userInfo.getGroupId() != null) {
+						SET("groupId=#{groupId}");
+					}
+					if (userInfo.getStatus() != null) {
+						SET("status=#{status}");
+					}
+					if (userInfo.getFirmId() != null) {
+						SET("firmId=#{firmId}");
+					}
+					if (userInfo.getParentUserId() != null) {
+						SET("parentUserId=#{parentUserId}");
+					}
+					if (userInfo.getOperateId() != null) {
+						SET("operateId=#{operateId}");
+					}
+					WHERE("userId=#{userId}");
+				}
+			}
+		}.toString();
+	}
+
+	public String getAllUsers(UserInfo userInfo) {
+		return new SQL() {
+			{
+				SELECT("*").FROM("userinfo");
+				if (userInfo.getUserNo() != null) {
+					WHERE("userNo=#{userNo}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getUserName())) {
+					WHERE("userName=#{userName}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getPassword())) {
+					WHERE("password=#{password}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getMobilePhone())) {
+					WHERE("mobilePhone=#{mobilePhone}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getEmail())) {
+					WHERE("email=#{email}");
+				}
+				if (userInfo.getRoleId() != null) {
+					WHERE("roleId=#{roleId}");
+				}
+				if (StringUtils.isNotBlank(userInfo.getRoleName())) {
+					WHERE("roleName=#{roleName}");
+				}
+				if (userInfo.getGroupId() != null) {
+					WHERE("groupId=#{groupId}");
+				}
+				if (userInfo.getStatus() != null) {
+					WHERE("status=#{status}");
+				}
+				if (userInfo.getFirmId() != null) {
+					WHERE("firmId=#{firmId}");
+				}
+				if (userInfo.getParentUserId() != null) {
+					WHERE("parentUserId=#{parentUserId}");
+				}
+				if (userInfo.getOperateId() != null) {
+					WHERE("operateId=#{operateId}");
+				}
+			}
+		}.toString();
+	}
+
+	public String getUserById(Integer userId) {
+		return "SELECT * FROM  userinfo WHERE userId=#{userId}";
+	}
+
+	public String deleteUser(Integer userId) {
+		return "DELETE FROM userinfo WHERE userId=#{userId}";
+	}
+}
--- vending/src/test/java/com/vending/platform/service/test/AuthorityinfoTest.java
+++ vending/src/test/java/com/vending/platform/service/test/AuthorityinfoTest.java
@@ -2,83 +2,165 @@ package com.vending.platform.service.test;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Update;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.vending.platform.dao.IAuthoritysAndRolesDao;
+import com.lin.domain.User;
+import com.vending.platform.dao.IUserManagerDao;
 import com.vending.platform.domain.AuthorityInfo;
+import com.vending.platform.domain.RoleInfo;
+import com.vending.platform.domain.UserInfo;
 import com.vending.platform.exception.SQLFormatException;
 
 /** @author Miley_Ren */
 // 指定bean注入的配置文件
 @ContextConfiguration("classpath:spring-mybatis.xml")
-// 使用标准的JUnit @RunWith注释来告诉JUnit使用Spring TestRunner
 @RunWith(SpringJUnit4ClassRunner.class)
 public class AuthorityinfoTest {
 
-    @Autowired
-    private IAuthoritysAndRolesDao aDao;
+	@Autowired
+	private IUserManagerDao aDao;
 
-    @Test
-    public void insert() throws SQLFormatException {
-        System.out.println("test");
-        AuthorityInfo authorityInfo = new AuthorityInfo();
-        authorityInfo.setOperateId(1);
-        authorityInfo.setAuthName("测试权限2");
-        authorityInfo.setAuthCode("001");
-        // authorityInfo.setFirmId(1);
-        try {
-            aDao.insertAuthorityInfo(authorityInfo);
-        } catch (Exception e) {
-            throw new SQLFormatException("传入的参数不正确");
-        }
-    }
+	@Test
+	public void insetUser() throws SQLFormatException {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserNo("111");
+		userInfo.setUserName("123");
+		userInfo.setPassword("123");
+		userInfo.setRoleId(2);
+		userInfo.setStatus(1);
+		userInfo.setFirmId(2);
+		userInfo.setParentUserId(2);
+		aDao.insertUser(userInfo);
+	}
 
-    @Test
-    public void update() {
-        AuthorityInfo authorityInfo = new AuthorityInfo();
-        authorityInfo.setOperateId(1);
-        authorityInfo.setAuthName("修改权限2");
-        // authorityInfo.setAuthCode("001");
-        // authorityInfo.setFirmId(1);
-        authorityInfo.setAuthId(8);
-        try {
-            aDao.updateAutorityInfo(authorityInfo);
-        } catch (Exception e) {
-            System.out.println("插入数据不符合规定：" + e.getMessage());
-        }
-    }
+	@Test
+	public void UpdateUser() {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserId(5);
+		userInfo.setUserNo("12345");
+		userInfo.setUserName("update");
+		userInfo.setPassword("123");
+		userInfo.setRoleId(2);
+		userInfo.setStatus(1);
+		userInfo.setFirmId(2);
+		userInfo.setParentUserId(2);
+		userInfo.setMobilePhone("1232423");
+		aDao.updateUser(userInfo);
+	}
 
-    @Test
-    public void selectAll() {
-        AuthorityInfo authorityInfo = new AuthorityInfo();
-        authorityInfo.setAuthName("测试权限2");
-        List<AuthorityInfo> authorityInfos = aDao
-                .getAllAuthorityInfos(authorityInfo);
-        for (AuthorityInfo authorityInfo2 : authorityInfos) {
-            System.out.println(authorityInfo2.toString());
-        }
-    }
+	@Test
+	public void testGetUser() {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserName("update");
+		userInfo.setRoleId(2);
+		userInfo.setStatus(1);
+		List<UserInfo> userInfos = aDao.getAllUsers(userInfo);
+		for (UserInfo userInfo2 : userInfos) {
+			System.out.println(userInfo2.toString());
+		}
 
-    @Test
-    public void selectById() {
-        AuthorityInfo aInfo = aDao.selectAuthorityInfoById(5);
-        System.out.println(aInfo.toString());
-    }
+		System.out.println(aDao.getUserById(1).toString());
+	}
 
-    @Test
-    public void del() {
-        aDao.deleteAuthorityInfo(9);
-    }
+	@Test
+	public void insertRole() throws SQLFormatException {
+		RoleInfo roleInfo = new RoleInfo();
+		// roleInfo.setRoleName("test");
+		roleInfo.setAuthorityCode("ttt;");
+		roleInfo.setAuthorityName("ttt;");
+		roleInfo.setStatus(1);
+		// roleInfo.setFirmId(1);
+		// roleInfo.setOperateId(null);
+		try {
+			aDao.insertRoleInfo(roleInfo);
+		} catch (Exception e) {
+			System.out.println("输入格式不正确");
+		}
+	}
 
-    public void setaDao(IAuthoritysAndRolesDao aDao) {
-        this.aDao = aDao;
-    }
+	@Test
+	public void updateRole() {
+		RoleInfo roleInfo = new RoleInfo();
+		roleInfo.setRoleId(6);
+		roleInfo.setRoleName("testuPDATE5");
+		roleInfo.setAuthorityCode("ttt_UPDATE;");
+		roleInfo.setAuthorityName("ttt;");
+		roleInfo.setStatus(1);
+		aDao.updateRoleInfo(roleInfo);
+	}
 
-    public IAuthoritysAndRolesDao getaDao() {
-        return aDao;
-    }
+	@Test
+	public void getAllRoles() {
+		RoleInfo roleInfo = new RoleInfo();
+		roleInfo.setRoleName("testuPDATE5");
+		roleInfo.setAuthorityCode("ttt_UPDATE;");
+		roleInfo.setAuthorityName("ttt;");
+		roleInfo.setStatus(1);
+		List<RoleInfo> roleInfos = aDao.getAllRoles(roleInfo);
+		for (RoleInfo roleInfo2 : roleInfos) {
+			System.out.println(roleInfo2.toString());
+		}
+	}
+
+	@Test
+	public void getRoleById() {
+		System.out.println(aDao.getRoleById(2).toString());
+	}
+
+	@Test
+	public void insert() throws SQLFormatException {
+		System.out.println("test");
+		AuthorityInfo authorityInfo = new AuthorityInfo();
+		// authorityInfo.setOperateId(1);
+		authorityInfo.setAuthName("测试权限2");
+		authorityInfo.setAuthCode("001");
+		// authorityInfo.setFirmId(1);
+		try {
+			aDao.insertAuthorityInfo(authorityInfo);
+		} catch (Exception e) {
+			throw new SQLFormatException("传入的参数不完善");
+		}
+	}
+
+	@Test
+	public void update() {
+		AuthorityInfo authorityInfo = new AuthorityInfo();
+		authorityInfo.setOperateId(1);
+		authorityInfo.setAuthName("修改权限2");
+		// authorityInfo.setAuthCode("001");
+		// authorityInfo.setFirmId(1);
+		authorityInfo.setAuthId(8);
+		try {
+			aDao.updateAutorityInfo(authorityInfo);
+		} catch (Exception e) {
+			System.out.println("插入数据不符合规定：" + e.getMessage());
+		}
+	}
+
+	@Test
+	public void selectAll() {
+		AuthorityInfo authorityInfo = new AuthorityInfo();
+		authorityInfo.setAuthName("测试权限2");
+		List<AuthorityInfo> authorityInfos = aDao.getAllAuthorityInfos(authorityInfo);
+		for (AuthorityInfo authorityInfo2 : authorityInfos) {
+			System.out.println(authorityInfo2.toString());
+		}
+	}
+
+	@Test
+	public void selectById() {
+		AuthorityInfo aInfo = aDao.getAuthorityInfoById(5);
+		System.out.println(aInfo.toString());
+	}
+
+	@Test
+	public void del() {
+		aDao.deleteAuthorityInfo(9);
+	}
+
 }
