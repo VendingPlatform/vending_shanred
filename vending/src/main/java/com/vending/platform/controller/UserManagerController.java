588c28bcd90b6f49f2511850712d235ee63230ff
nannan <nannan@163.com>
2017-04-08 20:33:32 +0800

    添加权限管理

--- vending/src/main/java/com/vending/platform/controller/UserManagerController.java
+++ vending/src/main/java/com/vending/platform/controller/UserManagerController.java
@@ -1,6 +1,7 @@
 package com.vending.platform.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -23,115 +24,169 @@ import com.vending.platform.service.IUserManagerService;
 @SessionAttributes({ "user", "userAuth" })
 @RequestMapping("user")
 public class UserManagerController extends UtilsAction {
-    private static final Logger logger = Logger
-            .getLogger(UserManagerController.class);
-    private static final long serialVersionUID = 6934405543902610637L;
-    @Autowired
-    private IUserManagerService userManagerService;
+	private static final Logger logger = Logger.getLogger(UserManagerController.class);
+	private static final long serialVersionUID = 6934405543902610637L;
+	@Autowired
+	private IUserManagerService userManagerService;
 
-    @RequestMapping(value = "/getAllUsers")
-    public String getAllUsers(@ModelAttribute("user") UserInfo userInfo) {
-        Integer firmId = userInfo.getFirmInfo().getFirmId();
-        if (firmId == 0) {
-            // 管理员，查看所有用户
+	@RequestMapping(value = "/getAllUsers")
+	public String getAllUsers(@ModelAttribute("user") UserInfo userInfo) {
+		Integer firmId = userInfo.getFirmInfo().getFirmId();
+		if (firmId == 0) {
+			// 管理员，查看所有用户
 
-        } else if (firmId == 1) {
-            // 运营商，根据不同的角色查看用户
-        } else if (firmId == 2) {
-            // 查看厂商用户
-        }
-        return "";
-    }
+		} else if (firmId == 1) {
+			// 运营商，根据不同的角色查看用户
+		} else if (firmId == 2) {
+			// 查看厂商用户
+		}
+		return "";
+	}
 
-    @RequestMapping(value = "/getAllAuthoritys")
-    public String getAllAuthoritys(@ModelAttribute("user") UserInfo userInfo,
-            @ModelAttribute("userAuth") Set<AuthorityInfo> userAuth,
-            ModelMap modelMap) {
-        Integer firmType = userInfo.getFirmInfo().getFirmType();
-        AuthorityInfo authorityInfo = new AuthorityInfo();
-        if (firmType == 1) {
-            // 运营商
-            authorityInfo.setAuthType(1);
-        } else if (firmType == 2) {
-            // 厂商
-            authorityInfo.setAuthType(2);
-        }
-        List<AuthorityInfo> authorityInfos = userManagerService
-                .getAllAuthoritys(authorityInfo);
-        modelMap.addAttribute("authorityInfos", authorityInfos);
-        return "genview/AuthorityInfo";
-    }
+	@RequestMapping(value = "/getAllAuthoritys")
+	public String getAllAuthoritys(@ModelAttribute("user") UserInfo userInfo, ModelMap modelMap) {
+		Integer firmType = userInfo.getFirmInfo().getFirmType();
+		AuthorityInfo authorityInfo = new AuthorityInfo();
+		if (firmType == 1) {
+			// 运营商
+			authorityInfo.setAuthType(1);
+		} else if (firmType == 2) {
+			// 厂商
+			authorityInfo.setAuthType(2);
+		}
+		List<AuthorityInfo> authorityInfos = userManagerService.getAllAuthoritys(authorityInfo);
+		modelMap.addAttribute("authorityInfos", authorityInfos);
+		try {
+			writeJson(authorityInfos);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "genview/AuthorityInfo";
+	}
 
-    //// 还需根据不同权限进行查询，并不是所有人都可以查询用户角色
-    // 或者让用户可以查看，但是不要进行其他操作
-    @RequestMapping(value = "/getAllRoles")
-    public String getAllRoles(@ModelAttribute("user") UserInfo userInfo,
-            ModelMap modelMap) {
-        Integer firmType = userInfo.getFirmInfo().getFirmType();
-        RoleInfo roleInfo = new RoleInfo();
-        if (firmType != 0) {
-            roleInfo.setFirmType(firmType);
-        }
-        List<RoleInfo> roleInfos = userManagerService.getAllRoles(roleInfo);
-        modelMap.addAttribute("allRoleInfos", roleInfos);
-        return "genview/RoleInfo";
-    }
+	@RequestMapping(value = "/getAllAuths")
+	public String getAllAuths(@RequestParam("roleId") Integer roleId, @RequestParam("firmType") Integer firmType,
+			ModelMap modelMap) {
+		RoleAuthInfo authInfo = new RoleAuthInfo();
+		authInfo.setRoleId(roleId);
+		List<AuthorityInfo> authInfosAlready = new ArrayList<AuthorityInfo>();
+		for (RoleAuthInfo r : userManagerService.getAllRoleAuthInfos(authInfo)) {
+			authInfosAlready.add(r.getAuthorityInfo());
+		}
+		AuthorityInfo authorityInfo = new AuthorityInfo();
+		if (firmType != 0) {
+			authorityInfo.setAuthType(firmType);
+		}
+		List<AuthorityInfo> authorityInfos = userManagerService.getAllAuthoritys(authorityInfo);
+		List<AuthorityInfo> auths = new ArrayList<>();
+		for (AuthorityInfo a : authorityInfos) {
+			if(!authInfosAlready.contains(a)){
+				auths.add(a);
+			}
+		}
+		modelMap.addAttribute("auths", auths);
+		try {
+			writeJson(auths);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "genview/RoleInfo";
+	}
 
-    @RequestMapping(value = "/getRoleAuthInfo")
-    public String getRoleAuthInfo(@RequestParam("roleId") Integer roleId,
-            ModelMap modelMap) {
-        RoleAuthInfo roleAuthInfo = new RoleAuthInfo();
-        roleAuthInfo.setRoleId(roleId);
-        List<RoleAuthInfo> roleAuthInfos = userManagerService
-                .getAllRoleAuthInfos(roleAuthInfo);
-        modelMap.addAttribute("roleAuthInfos", roleAuthInfos);
+	//// 还需根据不同权限进行查询，并不是所有人都可以查询用户角色
+	// 或者让用户可以查看，但是不要进行其他操作
+	@RequestMapping(value = "/getAllRoles")
+	public String getAllRoles(@ModelAttribute("user") UserInfo userInfo, ModelMap modelMap) {
+		Integer firmType = userInfo.getFirmInfo().getFirmType();
+		RoleInfo roleInfo = new RoleInfo();
+		if (firmType != 0) {
+			roleInfo.setFirmType(firmType);
+		}
+		List<RoleInfo> roleInfos = userManagerService.getAllRoles(roleInfo);
+		modelMap.addAttribute("allRoleInfos", roleInfos);
+		return "genview/RoleInfo";
+	}
 
-        return "genview/RoleAuthInfo";
-    }
+	@RequestMapping(value = "/getRoleAuthInfo")
+	public String getRoleAuthInfo(@RequestParam("roleId") Integer roleId, ModelMap modelMap) {
+		RoleAuthInfo roleAuthInfo = new RoleAuthInfo();
+		roleAuthInfo.setRoleId(roleId);
+		List<RoleAuthInfo> roleAuthInfos = userManagerService.getAllRoleAuthInfos(roleAuthInfo);
+		modelMap.addAttribute("roleAuthInfos", roleAuthInfos);
+		return "genview/RoleAuthInfo";
+	}
 
-    @RequestMapping(value = "/insertRole")
-    public String insertRole(RoleInfo roleInfo) {
-        if (userManagerService.getAllRoles(roleInfo).size() > 0) {
-            try {
-                write("角色名被占用，请更换角色名重试。");
-                return "genview/RoleInfo";
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        userManagerService.insertRole(roleInfo);
-        try {
-            write("添加成功。");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "genview/RoleInfo";
-    }
+	@RequestMapping(value = "/insertRole")
+	public String insertRole(RoleInfo roleInfo) {
+		if (userManagerService.getAllRoles(roleInfo).size() > 0) {
+			try {
+				write("角色名被占用，请更换角色名重试。");
+				return "genview/RoleInfo";
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		userManagerService.insertRole(roleInfo);
+		try {
+			write("添加成功。");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "genview/RoleInfo";
+	}
 
-    @RequestMapping(value = "/getRoleById")
-    public String getRoleById(@RequestParam("roleId") Integer roleId,
-            ModelMap modelMap) {
-        RoleInfo roleInfo = userManagerService.getRoleById(roleId);
-        modelMap.addAttribute("roleInfo", roleInfo);
-        try {
-            writeJson(roleInfo);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "genview/RoleInfo";
-    }
+	@RequestMapping(value = "/getRoleById")
+	public String getRoleById(@RequestParam("roleId") Integer roleId, ModelMap modelMap) {
+		RoleInfo roleInfo = userManagerService.getRoleById(roleId);
+		modelMap.addAttribute("roleInfo", roleInfo);
+		try {
+			writeJson(roleInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "genview/RoleInfo";
+	}
 
-    @RequestMapping(value = "/updateRole")
-    public String updateRole(RoleInfo roleInfo) {
-        userManagerService.updateRole(roleInfo);
-        return "genview/RoleInfo";
-    }
+	@RequestMapping(value = "/assignAuthToRole")
+	public String assignAuthToRole(@RequestParam("authIds") Integer[] authIds, @RequestParam("roleId") Integer roleId) {
+		RoleAuthInfo roleAuthInfo = new RoleAuthInfo();
+		for (Integer authId : authIds) {
+			roleAuthInfo.setRoleId(roleId);
+			roleAuthInfo.setAuthId(authId);
+			userManagerService.insertRoleAuthInfo(roleAuthInfo);
+		}
+		return "genview/RoleInfo";
+	}
 
-    @RequestMapping(value = "/deleteRole")
-    public String deleteRole(@RequestParam("roleId") Integer roleId) {
-        // 删除角色
-        userManagerService.deleteRole(roleId);
-        logger.debug("删除角色成功");
-        return "genview/RoleInfo";
-    }
+	@RequestMapping(value = "/updateRole")
+	public String updateRole(RoleInfo roleInfo) {
+		userManagerService.updateRole(roleInfo);
+		return "genview/RoleInfo";
+	}
+
+	@RequestMapping(value = "/deleteRole")
+	public String deleteRole(@RequestParam("roleId") Integer roleId) {
+		RoleAuthInfo roleAuthInfo = new RoleAuthInfo();
+		roleAuthInfo.setRoleId(roleId);
+		List<RoleAuthInfo> roleAuths = userManagerService.getAllRoleAuthInfos(roleAuthInfo);
+		for (RoleAuthInfo roleAuth : roleAuths) {
+			String authCode = roleAuth.getAuthorityInfo().getAuthCode();
+			if (authCode.equals("000") || authCode.equals("001") || authCode.equals("002")) {
+				try {
+					write("无法删除该角色");
+					return "genview/RoleInfo";
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 删除角色
+		userManagerService.deleteRole(roleId);
+		try {
+			write("删除角色成功");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "genview/RoleInfo";
+	}
 }
--- vending/src/main/webapp/viewpage/genview/RoleInfo.jsp
+++ vending/src/main/webapp/viewpage/genview/RoleInfo.jsp
@@ -1,15 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet"
-	href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
-<script
-	src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Role Info Page</title>
 </head>
 <body>
@@ -26,8 +23,7 @@
 		</div>
 		<!-- firmType==0系统管理员才有的权限 -->
 		<c:if test="${user.firmInfo.firmType==0 }">
-			<a type="button" class="btn btn-primary" data-toggle="modal"
-				data-target="#insertRole">添加角色</a>
+			<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#insertRole">添加角色</a>
 		</c:if>
 		<table class="table">
 			<tr>
@@ -53,17 +49,15 @@
 						</c:if>
 					</td>
 					<td>${role.operateDate }</td>
-					<td><a href="<c:url value="/user/getRoleAuthInfo"/>?roleId=${role.roleId }" class="btn default">
-							<span class="glyphicon glyphicon-info-sign" title="详情"></span> 
-						</a> 
-						<a class="btn default" onclick="getRoleById(${role.roleId })" data-toggle="modal" data-target="#getRole"> 
+					<td>
+						<a href="<c:url value="/user/getRoleAuthInfo"/>?roleId=${role.roleId }" class="btn default">
+							 <span class="glyphicon glyphicon-info-sign" title="详情"></span>
+						</a> <a class="btn default" onclick="getRoleById(${role.roleId })" data-toggle="modal" data-target="#getRole"> 
 							<span class="glyphicon glyphicon-edit" title="编辑"></span>
-						</a> 
-						<a  href="<c:url value="/user/insertRoleAuthInfo"/>?roleId=${role.roleId }" class="btn default">
-							<span class="glyphicon glyphicon-plus" title="添加权限"></span> 
-						</a> 
-						<a onclick="delRole(${role.roleId })" class="btn default">
-							<span class="glyphicon glyphicon-trash" title="删除角色"></span> 
+						</a> <a class="btn default" onclick="getAllAuths(${role.roleId },${role.firmType})" data-toggle="modal" data-target="#getAuths"> 
+							<span class="glyphicon glyphicon-plus" title="添加权限"></span>
+						</a> <a onclick="delRole(${role.roleId })" class="btn default"> 
+							<span class="glyphicon glyphicon-trash" title="删除角色"></span>
 						</a>
 					</td>
 				</tr>
@@ -71,13 +65,11 @@
 		</table>
 	</div>
 
-	<div class="modal fade" id="insertRole" tabindex="-1" role="dialog"
-		aria-labelledby="myModalLabel">
+	<div class="modal fade" id="insertRole" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal"
-						aria-label="Close">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 					<h4 class="modal-title" id="myModalLabel">添加角色</h4>
@@ -85,9 +77,8 @@
 				<div class="modal-body" style="width: 80%; padding-left: 30px">
 					<form class="form" id="insertRoleForm">
 						<div class="form-group">
-							<label for="roleName">角色名称:</label> <input type="text"
-								class="form-control" name="roleName" placeholder="输入角色名称"
-								required="required">
+							<label for="roleName">角色名称:</label>
+							<input type="text" class="form-control" name="roleName" placeholder="输入角色名称" required="required">
 						</div>
 						<label for="firmType">角色类型:</label>
 						<select name="firmType" class="form-control" required>
@@ -95,13 +86,11 @@
 							<option value="1" id="ope">运营商</option>
 							<option value="2" id="manu">厂商</option>
 						</select>
-
 					</form>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-					<button type="button" class="btn btn-primary"
-						onclick="insertRole()" data-dismiss="modal">保存</button>
+					<button type="button" class="btn btn-primary" onclick="insertRole()" data-dismiss="modal">保存</button>
 				</div>
 			</div>
 		</div>
@@ -117,24 +106,81 @@
 				</div>
 				<div class="modal-body" style="width: 80%; padding-left: 30px">
 					<form class="form" id="updateRoleForm">
-					<input type="hidden" name="roleId" id="roleIdUpdate">
+						<input type="hidden" name="roleId" id="roleIdUpdate">
 						<div class="form-group">
-							<label for="roleName">角色名称:</label> 
+							<label for="roleName">角色名称:</label>
 							<input type="text" class="form-control" name="roleName" id="newRoleName" placeholder="输入角色名称" required="required">
 						</div>
-						<label for="firmType" id="type">角色类型:</label> 
+						<label for="firmType" id="type">角色类型:</label>
 					</form>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-					<button type="button" class="btn btn-primary"
-						onclick="updateRole()" data-dismiss="modal">保存</button>
+					<button type="button" class="btn btn-primary" onclick="updateRole()" data-dismiss="modal">保存</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="getAuths" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h4 class="modal-title" id="myModalLabel">添加权限</h4>
+				</div>
+				<div class="modal-body" style="width: 80%; padding-left: 40px">
+					<form class="form" id="authAssignForm">
+						<input type="hidden" name="roleId" id="roleIdAssign">
+						<div id="checkbox">
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+					<button type="button" class="btn btn-primary" onclick="authAssignedRole()" data-dismiss="modal">保存</button>
 				</div>
 			</div>
 		</div>
 	</div>
 </body>
 <script type="text/javascript">
+function authAssignedRole(){
+	$.ajax({
+		url : "<c:url value='/user/assignAuthToRole'/>",
+		type: "post",
+		dataType:"text",
+		data: $('#authAssignForm').serialize(),
+		success:function(response){
+			alert("分配成功");
+        	location.reload();
+        },  
+        error:function(){  
+            alert("获取数据失败");  
+        }  
+	});
+}
+function getAllAuths(roleId,firmType){
+	$.ajax({
+		url : "<c:url value='/user/getAllAuths'/>",
+		type: "get",
+		dataType:"json",
+		data: {roleId:roleId,firmType:firmType},
+		success:function(response){
+			$("#roleIdAssign").val(roleId);
+			$("#checkbox").empty();
+			for(var i=0;i<response.length;i++){
+				var html ="<label class='checkbox'><input type='checkbox' name='authId' value='"+response[i].authId+"'>"+response[i].authName+"&nbsp;"+response[i].authCode+"&nbsp;"+response[i].authDesc+"</label>";
+				$("#checkbox").append(html);
+			}
+        },  
+        error:function(){  
+            alert("获取数据失败");  
+        }  
+	});
+}
+
 function delRole(roleId){
 	if(confirm("确认删除该角色，及该角色的权限对应关系？")){
 		$.ajax({
@@ -142,7 +188,7 @@ function delRole(roleId){
 			type: "delete",
 			dataType:"text",
 			success:function(response){
-				alert("删除成功");
+				alert(response);
 	        	location.reload();
 	        },  
 	        error:function(){  
